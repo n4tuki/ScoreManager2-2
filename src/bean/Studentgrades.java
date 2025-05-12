46c17851d67df909db257054dd736cb1bca10664
kanouYoshiki <kmm2459345@stu.o-hara.ac.jp>
2025-05-12 11:42:19 +0900

    Studentgrades.java

---
+++ src/bean/Studentgrades.java
@@ -0,0 +1,57 @@
+package bean;
+public class Studentgrades {
+    private String no;
+    private String name;
+    private Integer entYear;
+    private String classNum;
+    private String isAttend;
+    private String schoolCd;
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getEntYear() {
+        return entYear;
+    }
+
+    public void setEntYear(Integer entYear) {
+        this.entYear = entYear;
+    }
+
+    public String getClassNum() {
+        return classNum;
+    }
+
+    public void setClassNum(String classNum) {
+        this.classNum = classNum;
+    }
+
+    public String getIsAttend() {
+        return isAttend;
+    }
+
+    public void setIsAttend(String isAttend) {
+        this.isAttend = isAttend;
+    }
+
+    public String getSchoolCd() {
+        return schoolCd;
+    }
+
+    public void setSchoolCd(String schoolCd) {
+        this.schoolCd = schoolCd;
+    }
+}
